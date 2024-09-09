3b148eee763b99eee1318e3558abec0d55f20692
John <oldtimersoffer@gmail.com>
2024-09-09 18:23:38 +0200

    change logic

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -11,12 +11,12 @@ pub enum WebSocketMessageType {
 #[derive(Serialize, Deserialize)]
 pub struct WebSocketMessage {
     pub message_type: WebSocketMessageType,
-    pub message: Option<ChatMessages>,
+    pub message: Option<ChatMessage>,
     pub users: Option<Vec<String>>,
     pub username: Option<String>,
 }
 #[derive(Serialize, Deserialize, Clone, PartialEq)]
-pub struct ChatMessages {
+pub struct ChatMessage {
     pub message: String,
     pub author: String,
     pub created_at: NaiveDateTime,
--- frontend/src/send_dialog.rs
+++ frontend/src/send_dialog.rs
@@ -1,10 +1,11 @@
-use web_sys::HtmlTextAreaElement;
+use web_sys::{HtmlInputElement, HtmlTextAreaElement};
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub send_message_callback: Callback<String>,
     pub username: String,
+    pub change_username_callback: Callback<String>,
 }
 
 #[function_component(SendDialog)]
@@ -12,8 +13,14 @@ pub fn send_dialog(props: &Props) -> Html {
     let new_message_handle = use_state(String::default);
     let new_message = (*new_message_handle).clone();
 
+    let new_username_handle = use_state(|| props.username.clone());
+    let new_username = (*new_username_handle).clone();
+
+    let is_editing_username_handle = use_state(bool::default);
+    let is_editing_username = (*is_editing_username_handle).clone();
+
     let cloned_new_message_handle = new_message_handle.clone();
-    let on_mesage_change = Callback::from(move |e: Event| {
+    let on_message_change = Callback::from(move |e: Event| {
         let target = e.target_dyn_into::<HtmlTextAreaElement>();
         if let Some(textarea) = target {
             cloned_new_message_handle.set(textarea.value());
@@ -27,12 +34,51 @@ pub fn send_dialog(props: &Props) -> Html {
         new_message_handle.set("".to_string());
     });
 
+    let cloned_is_editing_handle = is_editing_username_handle.clone();
+    let callback = props.change_username_callback.clone();
+    let cloned_new_username = new_username.clone();
+    let on_username_apply_click = Callback::from(move |_: MouseEvent| {
+        callback.emit(cloned_new_username.clone());
+        cloned_is_editing_handle.set(false);
+    });
+
+    let on_username_change = Callback::from(move |e: Event| {
+        let target = e.target_dyn_into::<HtmlInputElement>();
+        if let Some(input) = target {
+            new_username_handle.set(input.value());
+        }
+    });
+
+    let cloned_is_editing_handle = is_editing_username_handle.clone();
+    let on_username_change_click = Callback::from(move |_: MouseEvent| {
+        cloned_is_editing_handle.set(true);
+    });
+
+    let cloned_is_editing_handle = is_editing_username_handle.clone();
+    let on_username_edit_cancel_click = Callback::from(move |_: MouseEvent| {
+        cloned_is_editing_handle.set(false);
+    });
+
     html! {
-    <div class="input-group">
-            <button class="btn-secondary">{props.username.clone()}</button>
-            <span class="input-group-text">{"Your message: "}</span>
-            <textarea class="form-control" onchange={on_mesage_change} value={new_message}></textarea>
-            <button type="submit" class="btn-primary" onclick={on_button_click}>{"Send"}</button>
-            </div>
+       <div class="input-group">
+                if is_editing_username {
+                    <input type="text" class="form-control" onchange={on_username_change} value={new_username} />
+                    <button class="btn btn-secondary" onclick={on_username_apply_click}>
+                        {"✓"}
+                    </button>
+                    <button class="btn btn-danger" onclick={on_username_edit_cancel_click}>
+                        {"x"}
+                    </button>
+                } else {
+                    <button class="btn btn-secondary" onclick={on_username_change_click}>
+                        {props.username.clone()}
+                    </button>
+                }
+                    <span class="input-group-text">{"Your message:"}</span>
+                    <textarea class="form-control" onchange={on_message_change} value={new_message}></textarea>
+                    <button type="submit" class="btn-primary" onclick={on_button_click}>
+                        {"Send"}
+                    </button>
+        </div>
     }
 }
